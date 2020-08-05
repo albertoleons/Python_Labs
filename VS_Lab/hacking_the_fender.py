37317f06a5451feb9ee4620db5ff738a8052af33
Alberto Leon <alberto.leon@icloud.com>
2020-08-05 17:53:50 -0400

    Added passwords.csv

--- VS_Lab/hacking_the_fender.py
+++ VS_Lab/hacking_the_fender.py
@@ -4,22 +4,22 @@ import json
 compromised_users = []
 
 with open("passwords.csv") as password_file:
-  password_csv = csv.DictReader(password_file)
-  for password_row in password_csv:
-    compromised_users.append(password_row["Username"])
-    
-#print(compromised_users)
+    password_csv = csv.DictReader(password_file)
+    for password_row in password_csv:
+        compromised_users.append(password_row["Username"])
+
+# print(compromised_users)
 
 with open("compromised_users.txt", "a") as compromised_user_file:
-  for users in compromised_users:
-    compromised_user_file.write(users + "\n")
+    for users in compromised_users:
+        compromised_user_file.write(users + "\n")
 
 with open("boss_message.json", "w") as boss_message:
-  boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
-  json.dump(boss_message_dict, boss_message)
+    boss_message_dict = {"recipient": "The Boss", "message": "Mission Success"}
+    json.dump(boss_message_dict, boss_message)
 
 with open("new_passwords.csv", "w") as new_passwords_obj:
-  slash_null_sig = """
+    slash_null_sig = """
  _  _     ___   __  ____             
 / )( \   / __) /  \(_  _)            
 ) \/ (  ( (_ \(  O ) )(              
@@ -39,23 +39,4 @@ with open("new_passwords.csv", "w") as new_passwords_obj:
 
 """
 
-  new_passwords_obj.write(slash_null_sig)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    new_passwords_obj.write(slash_null_sig)
