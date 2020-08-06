cb7a15b1a70d5253bd5ea2a10beaca46ad91f9a8
Alberto Leon <alberto.leon@icloud.com>
2020-08-05 20:35:12 -0400

    init

--- CBT_DevNet/netconf ENCOR.py
+++ CBT_DevNet/netconf ENCOR.py
@@ -2,8 +2,8 @@ from ncclient import manager
 # import logging
 # logging.basicConfig(level=logging.DEBUG)
 
-router = {"host": "172.16.1.81", "port": "830",
-          "username": "cisco", "password": "cisco"}
+router = {"host": "192.168.1.41", "port": "830",
+          "username": "alberto", "password": "luna"}
 
 with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
     for capability in m.server_capabilities:
